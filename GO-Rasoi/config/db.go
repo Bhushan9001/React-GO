07596b409dc00d3f4be65957d899e0c0f2d64f32
bhushan9001 <bhushanpawar9001@gmail.com>
2024-11-09 12:02:39 +0530

    minor changes

--- GO-Rasoi/config/db.go
+++ GO-Rasoi/config/db.go
@@ -6,7 +6,6 @@ import (
 	"os"
 
 	"github.com/Bhushan9001/Go-Rasoi/internal/models"
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -15,9 +14,9 @@ var DB *gorm.DB
 
 func ConnectDB() {
 	// Load the .env file
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file: ", err)
-	}
+	// if err := godotenv.Load(); err != nil {
+	// 	log.Fatal("Error loading .env file: ", err)
+	// }
 
 	// Get environment variables
 	dbUser := os.Getenv("DB_USER")
@@ -28,8 +27,7 @@ func ConnectDB() {
 
 	// Construct the DSN (Data Source Name)
 	// dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
-	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=require TimeZone=Asia/Kolkata",dbHost,dbUser,dbPassword,dbName,dbPort);
-
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=require TimeZone=Asia/Kolkata", dbHost, dbUser, dbPassword, dbName, dbPort)
 
 	// Connect to the database using GORM
 	var err error
@@ -39,7 +37,7 @@ func ConnectDB() {
 	}
 
 	// AutoMigrate the models
-	if err := DB.AutoMigrate(&models.User{},&models.Recipe{} ,&models.Comment{},&models.Ingredient{},&models.Like{}); err != nil {
+	if err := DB.AutoMigrate(&models.User{}, &models.Recipe{}, &models.Comment{}, &models.Ingredient{}, &models.Like{}); err != nil {
 		log.Fatal("Error during auto migration: ", err)
 	}
 
